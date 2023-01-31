c2837ed443c9050cd83559d7d27bbb5489a0a83c
Chris <chrisagilbert.cg@gmail.com>
2023-01-31 05:11:39 +0000

    Decided to skip on 'handleable exceptions' at this time. Wrapper function complete, and to be merged into main.

--- utils.py
+++ utils.py
@@ -16,14 +16,9 @@ def log(function):
 		logging.debug(f"{datetime.now()} Function: * {function.__name__} * from: * {function.__globals__.get('__file__')} *")
 		logging.info(f"{datetime.now()} Attempting {function.__name__}.")
 		try: # For unhandleable exceptions
-			try: # Handle handleable exceptions here:
-				function(*args, **kwargs)
-			"""Exceptions are associated with each function represented as a tuple of handled exceptions
-				Handled exceptions result in warning messages and changed attribute values"""
-			except (e for e in function._handled_exceptions) as handled_exception:
-				logging.warning("Exception handled.")
-		except: # For unhandleable exceptions
-			logging.critical(f"*Critical error* * {datetime.now()} * with * {function.__name__} * due to")
+			function(*args, **kwargs)
+		except Exception as e: # For unhandleable exceptions
+			logging.critical(f"*Critical error* * {datetime.now()} * with * {function.__name__} * due to {e}")
 			raise
 	
 	return log
