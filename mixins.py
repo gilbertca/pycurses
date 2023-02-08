65077bfb57cae30fc333e07cead4a6539fed54ad
Chris <chrisagilbert.cg@gmail.com>
2023-02-08 05:07:25 +0000

    Moved mixin ideas to mixins.py

---
+++ mixins.py
@@ -0,0 +1,12 @@
+class Focusable:
+	"""
+	An inheritable abtract class which allows a window object to take focus
+	"""
+	pass
+
+class Scrollable:
+	"""
+	An inheritable abstract class which allows a window object to scroll
+	Objects should override the .draw() method
+	"""
+	pass
