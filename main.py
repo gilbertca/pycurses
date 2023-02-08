66e41d0d89d7d7bb8f0ba9fa654dc2f09ce06215
Chris <chrisagilbert.cg@gmail.com>
2023-02-08 05:08:00 +0000

    Removed mixin ideas from main.py

--- main.py
+++ main.py
@@ -4,18 +4,7 @@ import logging
 from utils import parse_json, log
 from baseview import AbstractBaseView
 
-class Focusable:
-	"""An inheritable abtract class which allows a window object to take focus"""
-	pass
-
-class Scrollable:
-	"""
-	An inheritable abstract class which allows a window object to scroll
-	Objects should override the .draw() method
-	"""
-	pass
-
-class ListView(AbstractBaseView):
+lass ListView(AbstractBaseView):
 	"""
 	A class to display a list given an arbitrary keyword list of attributes.
 	A list of keywords can be found in 'keywords.txt'
