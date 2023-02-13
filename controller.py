c3ea5649aadf74502fb9d7e6f4cfe248e1e10a87
Chris <chrisagilbert.cg@gmail.com>
2023-02-13 07:40:24 +0000

    Added basics to Controller in controller.py.

--- controller.py
+++ controller.py
@@ -1,17 +1,17 @@
-from main import ListView
-
+from listview import ListView
+from baseview import AbstractBaseView
 
 class Controller:
 	"""
 	Controller needs to map colors in order to keep track of arbitrary number of color pairs.
+	Controller needs to call draw methods for view objects
 	"""
-	def __init__(self, **views):
-		self.view_dict = {}
-		for view in views:
-			self.view_list.update(view)
+	def __init__(self, views_dict=None):
+		self.views_dict = views_dict
+		self.views = self.views_dict.get # Saves typing .get on every reference
 
-	def add_view(self, view):
-		self.view_list.update(view)
+	def add_view(self, view_dict):
+		self.views_dict.update(view_dict)
 
-	def map_colors(self, colors):
+	def map_colors(self):
 		pass
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import curses
 from listview import ListView
+from controller import Controller
 from utils import parse_json
 
 JSON_FILE1 = "json/listview1.json"
@@ -10,10 +11,14 @@ def main(stdscr):
 	"""
 	listatr1 = parse_json(JSON_FILE1)
 	listatr2 = parse_json(JSON_FILE2)
-	listview = ListView(listatr1)
-	listview.screen.getch()
+	listview1 = ListView(listatr1)
 	listview2 = ListView(listatr2)
-	listview2.screen.getch()
+	listviewdict = {
+		"listview1" : listview1,
+		"listview2" : listview2
+	}
+	controller = Controller(listviewdict)
+	listview1.screen.getch()
 	return 0
 
 if __name__ == "__main__":
