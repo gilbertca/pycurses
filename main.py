83c80b3ff20c9bfb0fb942381551c2f7521b4496
Chris <chrisagilbert.cg@gmail.com>
2023-02-20 16:42:53 +0000

    Misc. comments.

--- main.py
+++ main.py
@@ -15,9 +15,13 @@ def main(stdscr):
 	view_name = "listview"
 	view_atr = parse_json(JSON_FILE1)
 	# To draw views:
+	# TODO: Condense these 2 calls into 1 shortcut call
 	controller.create_view(view_name, view_atr, ListView)
 	controller.draw_view("listview")
+	# This line is to pause before ending. From here,
+	#	control should be handed off and allow interactibility
 	controller.get_view("listview").screen.getch()
+	# Returning *anything* ends a curses session.
 	return 0
 
 if __name__ == "__main__":
