80a7622b0181124e90c0172b9f083dd45cfd806c
Chris <chrisagilbert.cg@gmail.com>
2023-02-24 00:03:03 +0000

    Layed the ideas for the *interact* function.

--- main.py
+++ main.py
@@ -20,17 +20,17 @@ def main(stdscr):
 	# TODO: Condense these 2 calls into 1 shortcut call
 	controller.create_view(view_name, view_atr, ListView)
 	controller.create_view(view_name2, view_atr2, ListView)
-	controller.draw_view("listview")
-	controller.draw_view("listview2")
+	return controller.begin()
+	#controller.draw_view("listview")
+	#controller.draw_view("listview2")
 	# This line is to pause before ending. From here,
 	#	control should be handed off and allow interactibility
-	key_pressed = controller.get_view("listview").screen.getch()
-	if key_pressed == ord("x"):
-		curses.napms(1000)
+	#key_pressed = controller.get_view("listview").screen.getch()
+	#if key_pressed == ord("x"):
+	#	curses.napms(1000)
 	# Returning *anything* ends a curses session.
 	#	Perhaps this should be return Controller.run()
 	#	or something along those lines.
-	return 0
 
 if __name__ == "__main__":
 	curses.wrapper(main)
