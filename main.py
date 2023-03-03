51bfb44780ca9b897d3444b12e81057d37a08224
Chris <chrisagilbert.cg@gmail.com>
2023-03-03 06:31:14 +0000

    Changed main.py to reflect the sample game.

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from listview import ListView
 from controller import Controller
 from utils import parse_json
 
-JSON_FILE1 = "json/listview1.json"
-JSON_FILE2 = "json/listview2.json"
+JSON_FILE1 = "json/gameview.json"
 def main(stdscr):
 	"""
 	A method for testing the view
@@ -12,13 +11,10 @@ def main(stdscr):
 	# Required:
 	controller = Controller(stdscr)
 	# Arbitrary data:
-	view_name = "listview"
-	view_name2 = "listview2"
+	view_name = "gameview"
 	view_atr = parse_json(JSON_FILE1)
-	view_atr2 = parse_json(JSON_FILE2)
 	# To draw views:
 	controller.create_view(view_name, view_atr, ListView)
-	controller.create_view(view_name2, view_atr2, ListView)
 	# Program ends upon returning 0:
 	return controller.begin()
 
